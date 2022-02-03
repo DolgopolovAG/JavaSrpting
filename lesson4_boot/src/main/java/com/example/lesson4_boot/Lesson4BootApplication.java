b60c1ad019f2d916c6c100c3284dff0c48c4b845
Dolgopolov <Dolgopolov@it-limon.ru>
2022-02-03 16:08:45 +0500

    base commit

---
+++ lesson4_boot/src/main/java/com/example/lesson4_boot/Lesson4BootApplication.java
@@ -0,0 +1,13 @@
+package com.example.lesson4_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson4BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson4BootApplication.class, args);
+    }
+
+}
---
+++ lesson4_boot/src/test/java/com/example/lesson4_boot/Lesson4BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson4_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson4BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
