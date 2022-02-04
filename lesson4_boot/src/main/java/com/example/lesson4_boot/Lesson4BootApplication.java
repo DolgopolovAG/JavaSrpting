c04ebf1e4e35c31afba312b2d18e93a3bcf1e76c
Dolgopolov <Dolgopolov@it-limon.ru>
2022-02-04 15:40:08 +0500

    sistem

--- lesson4_boot/src/main/java/com/example/lesson4_boot/Lesson4BootApplication.java
+++ lesson4_boot/src/main/java/com/example/lesson4_boot/Lesson4BootApplication.java
@@ -1,8 +1,12 @@
 package com.example.lesson4_boot;
 
+//import com.example.lesson4_boot.config.ProductProps;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+//@EnableConfigurationProperties(ProductProps.class)
 @SpringBootApplication
 public class Lesson4BootApplication {
 
