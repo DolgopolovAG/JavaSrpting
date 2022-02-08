a6c11632859e38c4461c8aff2377492f6f693951
Dolgopolov <Dolgopolov@it-limon.ru>
2022-02-08 19:20:16 +0500

    lesson 5 commit

--- lessons5/src/main/java/ru/gb/ShopApp.java
+++ lessons5/src/main/java/ru/gb/ShopApp.java
@@ -5,24 +5,61 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import ru.gb.config.HibernateConfig;
 import ru.gb.dao.ManufacturerDao;
 import ru.gb.dao.OldJdbcManufacturerDao;
+import ru.gb.dao.product.OldJdbcProductDao;
+import ru.gb.dao.product.ProductDao;
 import ru.gb.entity.Manufacturer;
+import ru.gb.entity.Product;
 
 public class ShopApp {
 
     public static void main(String[] args) {
-//        OldJdbcManufacturerDao oldJdbcManufacturerDao = new OldJdbcManufacturerDao();
-//
-//
-//        for (Manufacturer manufacturer : oldJdbcManufacturerDao.findAll()) {
-//            System.out.println(manufacturer);
+    //        OldJdbcManufacturerDao oldJdbcManufacturerDao = new OldJdbcManufacturerDao();
+    //
+    //
+    //        for (Manufacturer manufacturer : oldJdbcManufacturerDao.findAll()) {
+    //            System.out.println(manufacturer);
+    //        }
+
+//            AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HibernateConfig.class);
+//            ManufacturerDao manufacturerDao = context.getBean(ManufacturerDao.class);
+//            System.out.println(manufacturerDao.findById(3L));
+//            System.out.println(manufacturerDao.findNameById(5L));
+    //        for (Manufacturer manufacturer : manufacturerDao.findAll()) {
+    //            System.out.println(manufacturer);
+    //        }
+
+            //1
+    //        OldJdbcProductDao oldJdbcProductDaoDao = new OldJdbcProductDao();
+    //        for (Product pr : oldJdbcProductDaoDao.findAll()) {
+    //            System.out.println(pr);
+    //        }
+
+            //2
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DbConfig.class);
+//        ProductDao productDao = context.getBean(ProductDao.class);
+//        for (Product pr : productDao.findAll()) {
+//            System.out.println(pr);
 //        }
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HibernateConfig.class);
-        ManufacturerDao manufacturerDao = context.getBean(ManufacturerDao.class);
-        System.out.println(manufacturerDao.findById(3L));
-        System.out.println(manufacturerDao.findNameById(5L));
-//        for (Manufacturer manufacturer : manufacturerDao.findAll()) {
-//            System.out.println(manufacturer);
+            //3
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DbConfig.class);
+//        ProductDao productDao = context.getBean(ProductDao.class);
+//        System.out.println(productDao.findNameById(5L));
+
+            //4
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DbConfig.class);
+//        ProductDao productDao = context.getBean(ProductDao.class);
+//        System.out.println(productDao.findNameById(10L));
+//        System.out.println(productDao.findById(3L));
+//        for (Product pr : productDao.findAll()) {
+//            System.out.println(pr);
 //        }
+
+            //5
+         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HibernateConfig.class);
+         ProductDao productDao = context.getBean(ProductDao.class);
+         System.out.println(productDao.findNameById(10L));
+         System.out.println(productDao.findById(3L));
+
     }
 }
--- lessons5/src/main/java/ru/gb/entity/Product.java
+++ lessons5/src/main/java/ru/gb/entity/Product.java
@@ -3,6 +3,7 @@ package ru.gb.entity;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -12,9 +13,23 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 @ToString
+@Entity
+@Table(name = "PRODUCT")
+@NamedQueries({
+        @NamedQuery(name = "Product.findById",
+                query = "select m from Product m where m.id = :id"),
+        @NamedQuery(name = "Product.findNameById",
+                query = "select m.title from Product m where m.id = :id")
+})
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Long id;
+    @Column(name = "TITLE")
     private String title;
+    @Column(name = "COST")
     private BigDecimal cost;
+    @Column(name = "DATE")
     private LocalDate date;
 }
